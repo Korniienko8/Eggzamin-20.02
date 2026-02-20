019aca751ef87f5b42db5be343713c79d94b63b5
Korniienko8 <nastyakornienko8@gmail.com>
2026-02-20 13:21:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZGŁOSZENIA</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="naglowek">
+        <h1>ZGŁOSZENIA</h1>
+    </div>
+    <div class="main">
+        <div class="right"></div>
+        <div class="left">
+            <h2>Personel</h2>
+            <form method="post">
+                <input type="checkbox" checked name="Policjant">
+                <label for="Policjant">Policjant</label>
+                <input type="checkbox" checked name="Ratownik">
+                <label for="Ratownik">Ratownik</label>
+                <button type="submit">Pokaż</button>
+        </div>
+    </div>
+    <div class="footer"></div>
+
+
+</body>
+
+</html>
