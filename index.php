2feba58abf2ecc020d147b2d5884e01b7e7de0ec
Korniienko8 <nastyakornienko8@gmail.com>
2026-02-27 22:30:14 +0100

    Implement database connection and personnel query
    Added database connection and query to fetch personnel.

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
 </head>
 
 <body>
-    <div class="naglowek">
+    <?php
+    $server = "localhost";
+    $user = "root"; 
+    $password = "";
+    $database = "zgloszenia";
+    $connection = mysqli_connect($server, $user, $password, $database);
+    if ($connection) {
+        echo "database connection error";}
+    ?>
+    <div class="header">
         <h1>ZGŁOSZENIA</h1>
     </div>
     <div class="main">
-        <div class="right"></div>
+        <div class="right">
+            <h2>Nowe zgloszenie</h2>
+            <ol>
+            <?php
+            $connection = mysqli_connect($server, $user, $password, $database);
+            $query = "SELECT personel.id, personel.nazwisko FROM personel WHERE personel.id not in (SELECT rejestr.id_personel FROM rejestr)";
+            $result = mysqli_query($connection, $query);
+            echo mysqli_num_rows($result);
+            mysqli_close($connection);
+            ?>
+            <ol>
+        </div>
         <div class="left">
             <h2>Personel</h2>
             <form method="post">
@@ -22,9 +42,23 @@
                 <input type="checkbox" checked name="Ratownik">
                 <label for="Ratownik">Ratownik</label>
                 <button type="submit">Pokaż</button>
+                <table>
+                    <tr>
+                        <th>Id</th>
+                        <th>Imie</th>
+                        <th>Nazwisko</th>
+                    </tr>
+                    <?php
+                    ?>
+                </table>
+                <form> method="post">
+                    <input type="text" label="Wybierz id osoby z listy">
+                    <input type="number">
+                    <button type="submit">Dodaj zgloszenie</button>
+
         </div>
     </div>
-    <div class="footer"></div>
+    <div class="footer"> Strone wykonal: Ballerina Cappuccina</div>
 
 
 </body>
